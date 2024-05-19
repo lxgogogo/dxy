16bcab10bbc715dfe63cdd8a0b3358d58403171e
Leon <hepeichao@haodai.net>
2024-05-19 12:26:29 +0800

    message list update

--- lib/page/message/page_message_tab_child.dart
+++ lib/page/message/page_message_tab_child.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:holdem/model/comment_list.dart';
 import 'package:holdem/model/message.dart';
 import 'package:holdem/page/comment/item_comment.dart';
+import 'package:holdem/page/forum/page_forum_post_detail.dart';
+import 'package:holdem/page/index/page_article_detail.dart';
+import 'package:holdem/page/index/page_book_detail.dart';
+import 'package:holdem/page/index/page_video_detail.dart';
+import 'package:holdem/page/index/page_video_list.dart';
 import 'package:holdem/utils/net_request.dart';
 import 'package:holdem/utils/size_fit.dart';
 import 'package:holdem/widget/no_data.dart';
@@ -18,45 +24,56 @@ class MessageTabChildPage extends StatefulWidget {
 }
 
 class _MessageTabChildPageState extends State<MessageTabChildPage> {
-  List<String> items = ["1", "2", "3", "4", "5", "6", "7", "8"];
   List<MessageBean> messages = [];
   RefreshController _refreshController =
       RefreshController(initialRefresh: false);
   bool loaded = false;
+  int pageNum = 1;
 
   @override
   void initState() {
     super.initState();
+    reqListData();
+  }
+
+  reqListData() {
     NetRequest().messageList({
-      'pageNum': 1,
+      'pageNum': pageNum,
       'pageSize': 10,
       'filters': {'type': widget.type}
     }, (data) {
       List<MessageBean> dataList = List<MessageBean>.from(
           data['list'].map((comment) => MessageBean.fromJson(comment)));
+
       if (mounted) {
         setState(() {
-          messages = dataList;
-          loaded = true;
+          if (pageNum == 1) {
+            messages = dataList;
+            loaded = true;
+          } else {
+            messages.addAll(dataList);
+          }
         });
       }
+      _refreshController.loadComplete();
+      _refreshController.refreshCompleted();
     });
   }
 
   void _onRefresh() async {
     // monitor network fetch
-    await Future.delayed(Duration(milliseconds: 1000));
-    // if failed,use refreshFailed()
-    _refreshController.refreshCompleted();
+    setState(() {
+      pageNum = 1;
+    });
+    reqListData();
   }
 
   void _onLoading() async {
     // monitor network fetch
-    await Future.delayed(Duration(milliseconds: 1000));
-    // if failed,use loadFailed(),if no data return,use LoadNodata()
-    items.add((items.length + 1).toString());
-    if (mounted) setState(() {});
-    _refreshController.loadComplete();
+    setState(() {
+      pageNum++;
+    });
+    reqListData();
   }
 
   @override
@@ -64,7 +81,7 @@ class _MessageTabChildPageState extends State<MessageTabChildPage> {
     SizeFit.initialize(context);
     if (loaded && messages.length == 0) {
       return const Center(
-        child:  NoDataView(),
+        child: NoDataView(),
       );
     }
     return content();
@@ -84,6 +101,24 @@ class _MessageTabChildPageState extends State<MessageTabChildPage> {
         itemCount: messages.length,
       ),
     );
+  }
+
+  jumpPage(MessageBean bean) {
+    if (bean.jumpId == null) {
+      return;
+    }
+    int id = bean.jumpId!;
+    if (bean.jumpType == 'book') {
+      Get.to(BookDetailPage(id: id));
+    } else if (bean.jumpType == 'article') {
+      Get.to(ArticleDetailPage(id: id));
+    } else if (bean.jumpType == 'videoList') {
+      Get.to(VideoListPage(id: id));
+    } else if (bean.jumpType == 'video') {
+      Get.to(VideoDetailPage(id: id));
+    } else if (bean.jumpType == 'thread') {
+      Get.to(PostDetailPage(postId: id));
+    }
   }
 
   Widget messageCommentItem(MessageBean messageBean) {
@@ -156,147 +191,52 @@ class _MessageTabChildPageState extends State<MessageTabChildPage> {
                 SizedBox(
                   height: 10.px,
                 ),
-                Container(
-                    padding: EdgeInsets.only(left: 9.px),
-                    decoration: BoxDecoration(
-                        border: Border(
-                            left: BorderSide(
-                                width: 3.px,
-                                color: Colors.black.withOpacity(0.05)))),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        if (str.isNotEmpty)
-                          Text(
-                            str,
-                            style: TextStyle(
-                                color: Color(0xff333333), fontSize: 14.px),
-                          ),
-                        if (str.isNotEmpty)
-                          SizedBox(
-                            height: 8.px,
-                          ),
-                        Container(
-                          padding: EdgeInsets.all(10.px),
-                          decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.05),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10.px))),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text(messageBean.quote ?? '',
-                                  style: TextStyle(
-                                      color: Color(0xff666666),
-                                      fontSize: 14.px,
-                                      height: 2.0))
-                            ],
-                          ),
-                        )
-                      ],
-                    )),
-              ]))
-        ],
-      ),
-    );
-  }
-
-  Widget videoDataItem(int index) {
-    return Container(
-      padding: EdgeInsets.only(top: 13.px, bottom: 20.px),
-      margin: EdgeInsets.only(left: 16.px, right: 16.px),
-      decoration: BoxDecoration(
-          border: Border(
-              bottom: BorderSide(color: Color(0xffE5E5E5), width: 1.px))),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ClipOval(
-            child: Image.network(
-              'https://pic1.zhimg.com/80/v2-6545695ef3e3925dab264c68e54c23a0_1440w.webp',
-              width: 40.px,
-              height: 40.px,
-              fit: BoxFit.cover,
-            ),
-          ),
-          SizedBox(
-            width: 12.px,
-          ),
-          Expanded(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                Text(
-                  'data',
-                  style: TextStyle(
-                      color: const Color(0xff3B5078),
-                      fontSize: 13.px,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 5.px,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '@我的',
-                      style:
-                          TextStyle(color: Color(0xff666666), fontSize: 11.px),
-                    ),
-                    SizedBox(
-                      width: 8.px,
-                    ),
-                    Text('2024-1-1',
-                        style: TextStyle(
-                            color: Color(0xff999999), fontSize: 11.px))
-                  ],
-                ),
-                SizedBox(
-                  height: 10.px,
-                ),
-                Container(
-                    padding: EdgeInsets.only(left: 9.px),
-                    decoration: BoxDecoration(
-                        border: Border(
-                            left: BorderSide(
-                                width: 3.px,
-                                color: Colors.black.withOpacity(0.05)))),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          '@Ray 来看这篇文章',
-                          style: TextStyle(
-                              color: Color(0xff333333), fontSize: 14.px),
-                        ),
-                        SizedBox(
-                          height: 8.px,
-                        ),
-                        Container(
-                          padding: EdgeInsets.all(10.px),
-                          decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.05),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10.px))),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text('用户2：说的好回复评论说的好回复评论说的好回复评论',
-                                  style: TextStyle(
-                                      color: Color(0xff666666),
-                                      fontSize: 14.px,
-                                      height: 2.0))
-                            ],
-                          ),
-                        )
-                      ],
-                    )),
+                GestureDetector(
+                  onTap: () {
+                    jumpPage(messageBean);
+                  },
+                  child: Container(
+                      padding: EdgeInsets.only(left: 9.px),
+                      decoration: BoxDecoration(
+                          border: Border(
+                              left: BorderSide(
+                                  width: 3.px,
+                                  color: Colors.black.withOpacity(0.05)))),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          if (str.isNotEmpty)
+                            Text(
+                              str,
+                              style: TextStyle(
+                                  color: Color(0xff333333), fontSize: 14.px),
+                            ),
+                          if (str.isNotEmpty)
+                            SizedBox(
+                              height: 8.px,
+                            ),
+                          Container(
+                            padding: EdgeInsets.all(10.px),
+                            decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.05),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10.px))),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text(messageBean.quote ?? '',
+                                    style: TextStyle(
+                                        color: Color(0xff666666),
+                                        fontSize: 14.px,
+                                        height: 2.0))
+                              ],
+                            ),
+                          )
+                        ],
+                      )),
+                )
               ]))
         ],
       ),

1d3274e5aa7a0523c58cbc4c4cf87050a00fc4d2
liwei <liwei@eigpay.net>
2024-05-25 21:57:49 +0800

    首页tab请求问题修复

--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -18,7 +18,7 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> with AutomaticKeepAliveClientMixin{
+class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
   final List<Widget> _pages = [
     IndexPage(),
@@ -29,9 +29,6 @@ class _MainScreenState extends State<MainScreen> with AutomaticKeepAliveClientMi
 
   var actionEventBus;
 
-  @override
-  // TODO: implement wantKeepAlive
-  bool get wantKeepAlive => true;
 
   @override
   void initState() {
@@ -55,10 +52,7 @@ class _MainScreenState extends State<MainScreen> with AutomaticKeepAliveClientMi
       // appBar: AppBar(
       //   title: Text('TabBar Demo'),
       // ),
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _pages,
-      ),
+      body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         type: BottomNavigationBarType.fixed,

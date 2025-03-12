3e5ff1a74340fd1815422f8068a729e2b0cc9454
scroll <scroll@gmail>
2025-03-12 22:39:10 +0800

    refactor(page): 修改书籍详情页面标题并优化底部操作栏
    - 将书籍详情页面的标题从"详情"改为"书籍详情"- 优化了底部操作栏的布局结构
    - 调整了底部操作栏的样式，使其更加美观

--- lib/page/book_detail/book_detail_screen.dart
+++ lib/page/book_detail/book_detail_screen.dart
@@ -47,7 +47,7 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
         return Scaffold(
           appBar: CommonAppBar.arrowBack(
             context,
-            title: '详情',
+            title: '书籍详情',
             actions: [
               GestureDetector(
                 onTap: () {

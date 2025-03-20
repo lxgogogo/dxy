01c76a8f8b1aa9822bbffb1636771c1806d7575f
scroll <scroll@gmail.com>
2025-03-20 18:49:38 +0800

    fix: 打包

--- lib/widget/item_book.dart
+++ lib/widget/item_book.dart
@@ -29,8 +29,7 @@ class _BookItemState extends State<BookItem> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Get.toNamed(Routes.bookDetail, arguments: widget.article.id ?? 0)
-            ?.whenComplete(() {
+        Get.toNamed(Routes.bookDetail, arguments: widget.article.id ?? 0)?.whenComplete(() {
           widget.article.viewCount = (widget.article.viewCount ?? 0) + 1;
           setState(() {});
         });
@@ -56,65 +55,59 @@ class _BookItemState extends State<BookItem> {
             Container(
               clipBehavior: Clip.antiAlias,
               margin: EdgeInsets.only(right: 18.w),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(8.w))),
+              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(8.w))),
               child: CachedNetworkImage(
                 imageUrl: widget.article.cover ?? '',
                 width: 88.w,
                 fit: BoxFit.cover,
-                placeholder: (context, url) =>
-                    Assets.images.imageLoadingDef.image(fit: BoxFit.fill),
-                errorWidget: (context, url, error) =>
-                    Assets.images.imageLoadingDef.image(fit: BoxFit.fill),
+                placeholder: (context, url) => Assets.images.imageLoadingDef.image(fit: BoxFit.fill),
+                errorWidget: (context, url, error) => Assets.images.imageLoadingDef.image(fit: BoxFit.fill),
               ),
             ),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  SizedBox(
-                    height: 12.w,
-                  ),
+                  SizedBox(height: 8.w),
                   Text(
                     widget.article.title ?? '',
                     overflow: TextOverflow.ellipsis,
                     maxLines: 1,
                     style: TextStyle(
-                        color: '#333333'.hexColor,
-                        fontSize: 14.sp,
-                        height: 1.3),
+                      color: '#333333'.hexColor,
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w600,
+                      height: 1.2,
+                    ),
                   ),
-                  SizedBox(
-                    height: 4.w,
-                  ),
+                  SizedBox(height: 4.w),
                   Text(
                     '作者:${widget.article.author ?? ''}',
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                     style: TextStyle(
-                        color: '#333333'.hexColor.withOpacity(0.7),
-                        fontSize: 12.sp,
-                        height: 1.2),
+                      color: '#333333'.hexColor.withOpacity(0.7),
+                      fontSize: 12.sp,
+                      height: 1.2,
+                    ),
                   ),
+                  SizedBox(height: 4.w),
                   Text(
                     widget.article.description ?? '',
                     maxLines: 2,
                     overflow: TextOverflow.ellipsis,
                     style: TextStyle(
-                        color: '#333333'.hexColor.withOpacity(0.7),
-                        fontSize: 12.sp,
-                        height: 1.2),
-                  ),
-                  SizedBox(
-                    height: 10.w,
+                      color: '#333333'.hexColor.withOpacity(0.7),
+                      fontSize: 12.sp,
+                      height: 1.2,
+                    ),
                   ),
                   const Spacer(),
                   Row(
                     children: [
                       Text(
                         widget.article.createdAt != null
-                            ? DateUtil.formatDateAlias3(widget
-                                .article.createdAt!.millisecondsSinceEpoch)
+                            ? DateUtil.formatDateAlias3(widget.article.createdAt!.millisecondsSinceEpoch)
                             : '',
                         style: TextStyle(
                           fontSize: 12.sp,

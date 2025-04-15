c4df989192ed7cd9ded314688b8e6d1b486c9c28
scroll <scroll@gmail.com>
2025-04-15 10:48:36 +0800

    feat: 打包

---
+++ lib/page/terms_privacy/copy/terms_privacy_controller.dart
@@ -0,0 +1,48 @@
+part of 'terms_privacy_screen.dart';
+
+class TermsPrivacyController extends GetxController {
+  String url = '';
+  String title = '';
+  bool isLoading = true;
+
+  // void onAgree() {
+  //   Get.back(result: true);
+  // }
+
+  void onLoadStop() {
+    isLoading = false;
+    update();
+  }
+
+  // void onWebTitleChanged(String newTitle) {
+  //   if (newTitle.isEmpty || newTitle == title) return;
+  //   title = newTitle;
+  //   update();
+  // }
+
+  @override
+  void onInit() {
+    url = Get.arguments['url'] as String? ?? '';
+    title = Get.arguments['title'] as String? ?? '';
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    _prepareWebView();
+    super.onReady();
+  }
+
+  bool isCookieCleared = false;
+
+  Future<void> _prepareWebView() async {
+    final cookieManager = CookieManager();
+
+    await cookieManager.removeSessionCookies();
+    await cookieManager.deleteAllCookies();
+    await InAppWebViewController.clearAllCache();
+
+    isCookieCleared = true;
+    safeUpdate();
+  }
+}

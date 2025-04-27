60d256b56ad22c28c0e4bf4a85e1a11be8ad4f84
case <case@gmail.com>
2025-04-27 18:10:18 +0800

    fix:bug

--- lib/page/message/notice/message_notice_view.dart
+++ lib/page/message/notice/message_notice_view.dart
@@ -175,7 +175,7 @@ class _MessageNoticePageState extends State<MessageNoticePage> {
                           child: Text(
                             model.content ?? '',
                             overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
+                            maxLines: 1,
                             style: TextStyle(
                               color: index == 0
                                   ? ColorStyle.c333333

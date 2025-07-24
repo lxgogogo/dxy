8ee31854cf127a2aa7e3a8a693260805b319137c
case <case@gmail.com>
2025-07-24 08:28:56 +0800

    fix: ui

--- lib/widget/bottom_actions_view.dart
+++ lib/widget/bottom_actions_view.dart
@@ -235,7 +235,7 @@ class _CommonDetailBottomViewState extends State<CommonDetailBottomView> {
               ],
             ),
           ),
-          SizedBox(height: 15.w)
+          SafeArea(child: SizedBox(height: 0.1.w))
         ],
       ),
     );
